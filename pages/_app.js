b7e22d3befcb0bc79bdfaf5190f0ad4f54e9e5d3
ErikMoxley <exilerzero@yahoo.com>
2021-07-24 05:56:43 -0400

    added header and responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Header from '../components/Header'
+import HeaderTop from '../components/HeaderTop'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
+      <HeaderTop />
       <main className='container'>
         <Component {...pageProps} />
       </main>
