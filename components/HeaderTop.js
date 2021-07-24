e54239c7177f8e861d1660ed95f0eb54d1ee5193
ErikMoxley <exilerzero@yahoo.com>
2021-07-24 09:09:51 -0400

    updated npm run build error

--- components/HeaderTop.js
+++ components/HeaderTop.js
@@ -4,7 +4,7 @@ export default function HeaderTop() {
     <div className='header-container'>
       <div className='header-top'>
           <span className='header-text'><h1>Medium is a place to write, read, and connect</h1>
-          <h3>It's easy and free to post your thinking on any topic and connect with millions of readers.</h3>
+          <h3>It&apos;s easy and free to post your thinking on any topic and connect with millions of readers.</h3>
     </span>  </div>
     </div>
   )
