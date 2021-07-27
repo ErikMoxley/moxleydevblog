2db2cfbed5fed3016b1fbb630fe56affe5fe66be
ErikMoxley <exilerzero@yahoo.com>
2021-07-27 04:46:49 -0400

    removed footer for now, will have to adjust it later

---
+++ components/Footer.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <ul>
+      <div>
+        <li>
+        <Link href='/' passHref alt=''>
+        <h2><span>Blog</span></h2>
+        </Link></li>
+        <li>
+        <Link href='/contact' passHref alt=''>
+        <h2><span>Privacy</span></h2>
+        </Link></li>
+        <li>
+        <Link href='/about' passHref alt=''>
+        <h2><span>Terms</span></h2>
+        </Link></li>
+        <li>
+        <Link href='/' passHref alt=''>
+        <h2><span>About</span></h2>
+        </Link></li>
+      </div>
+      </ul>
+    </footer>
+  )
+}
