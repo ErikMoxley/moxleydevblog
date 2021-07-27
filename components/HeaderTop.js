3aeeec014a629017fea0479acf3060a5e5ab75ac
ErikMoxley <exilerzero@yahoo.com>
2021-07-27 03:11:14 -0400

    update

--- components/HeaderTop.js
+++ components/HeaderTop.js
@@ -1,11 +1,11 @@
 
-export default function HeaderTop() {
-  return (
-    <div className='header-container'>
-      <div className='header-top'>
-          <span className='header-text'><h1>Medium is a place to write, read, and connect</h1>
-          <h3>It&apos;s easy and free to post your thinking on any topic and connect with millions of readers.</h3>
-    </span>  </div>
-    </div>
-  )
-}
+// export default function HeaderTop() {
+//   return (
+//     <div className='header-container'>
+//       <div className='header-top'>
+//           <span className='header-text'><h1>Medium is a place to write, read, and connect</h1>
+//           <h3>It&apos;s easy and free to post your thinking on any topic and connect with millions of readers.</h3>
+//     </span>  </div>
+//     </div>
+//   )
+// }
