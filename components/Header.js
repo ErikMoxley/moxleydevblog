30ab7ff074574e77e9e464b51ccab0822b17f6d9
ErikMoxley <exilerzero@yahoo.com>
2021-07-31 05:44:10 -0400

    update

--- components/Header.js
+++ components/Header.js
@@ -6,20 +6,21 @@ export default function Header() {
       <ul className='nav-ul'>
         <span className='header-links'>
         <li className='nav-li'>
-        <Link href='/' passHref alt=''>
-        <h2 className='header-a'>Dev Blog</h2>
-        </Link></li>
-        <li className='nav-li'>
         <Link href='/personal' passHref alt=''>
-        <h2 className='header-a'>Personal Blog</h2>
+        <h2 className='header-a'>Blog</h2>
         </Link></li>
+        <a href='https://moxley.hashnode.dev/' passHref alt='' target="_blank" rel="noopener noreferrer">
+        <h2 className='header-a'>Hashnode</h2>
+        </a>
         <li className='nav-li'>
         <Link href='/about' passHref alt=''>
         <h2 className='header-a'>About</h2>
         </Link></li>
         <a href='https://www.erikmoxley.com/' passHref alt='' target="_blank" rel="noopener noreferrer">
         <h2 className='header-a'>Website</h2>
-        </a></span>
+        </a>
+        
+        </span>
       </ul>
     </header>
   )
