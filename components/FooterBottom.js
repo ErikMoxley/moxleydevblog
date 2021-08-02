a45b96e9819fe3330b46f0994615d1682bfb36dc
ErikMoxley <exilerzero@yahoo.com>
2021-08-02 07:01:13 -0400

    fixed footer responsive design

---
+++ components/FooterBottom.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import profilePic from '../public/images/newprofile.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCss3Alt, faHtml5, faJs, faReact, faNpm  } from "@fortawesome/free-brands-svg-icons";
+
+export default function FooterBottom() {
+  return (
+    <footer className='footer-bottom'>
+      <ul>
+          <span>
+            <li className="made-with">This blog was made with NextJS plus:</li>
+            <span>
+              <li className="fa-icons">
+              <span className="brand-icons"><FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon></span>
+              <span className="brand-icons"><FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon></span>
+              <span className="brand-icons"><FontAwesomeIcon icon={faJs}></FontAwesomeIcon></span>
+              <span className="brand-icons"><FontAwesomeIcon icon={faReact}></FontAwesomeIcon></span>
+              <span className="brand-icons"><FontAwesomeIcon icon={faNpm}></FontAwesomeIcon></span>
+              </li>
+            </span>
+            <li>
+        <p className="copyright"><h4>
+        &nbsp;&nbsp;Copyright &copy; {(new Date().getFullYear())} ErikMoxley</h4></p></li>
+         </span>
+      </ul>
+    </footer>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import '../styles/globals.css'
 import Footer from '../components/Footer'
+import FooterBottom from '../components/FooterBottom';
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
         </div>
       <Footer id='footer'/>
+      <FooterBottom id='footer-bottom'/>
       </div>
       </>
   )
