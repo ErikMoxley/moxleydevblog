b5b5de9ebaf2f4861948102e8589a9e9f07d1310
ErikMoxley <exilerzero@yahoo.com>
2021-08-03 11:38:16 -0400

    update

--- pages/contact.js
+++ pages/contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
           </ul>
 </div>
 
-<form role="form" method="POST" netlify-honeypot="bot-field" data-netlify-recaptcha="true" data-netlify="true">
+<form role="form" method="POST" netlify-honeypot="bot-field" data-netlify="true">
 <p className="hidden">
     <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
   </p>
@@ -46,7 +46,6 @@ const Contact = () => {
         </label> <br />
         <textarea name="message" id="yourmessage"></textarea>
       </p>
-      <div data-netlify-recaptcha="true"></div>
       <p>
         <button type="submit">Send</button>
       </p>
